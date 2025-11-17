14b8e80c5aa048c75c969380d7a92f7a72dad094
Anxious <126321376+WislUniversal@users.noreply.github.com>
2025-11-17 23:14:21 +0700

    Update AutoApplyIxpFastflags.bat

--- AutoApplyIxpFastflags.bat
+++ AutoApplyIxpFastflags.bat
@@ -2,7 +2,9 @@
 set "URL=https://raw.githubusercontent.com/WislUniversal/fastflagsroblox/refs/heads/main/fastflagstest.json"
 set "ClientSettingsDir=%LOCALAPPDATA%\Roblox\ClientSettings"
 set "DestinationPath=%ClientSettingsDir%\IxpSettings.json"
-
 if not exist "%ClientSettingsDir%" mkdir "%ClientSettingsDir%" >nul 2>&1
 powershell -Command "Invoke-WebRequest -Uri '%URL%' -OutFile '%DestinationPath%' -ErrorAction SilentlyContinue" >nul 2>&1
+if not exist "%DestinationPath%" (
+    type nul > "%DestinationPath%"
+)
 exit /b
